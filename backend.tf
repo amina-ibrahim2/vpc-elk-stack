936ffdb630562ef921eddd69a6572415b4f6ac8c
Amina Ibrahim <amina.ibrahim@cloudreach.com>
2022-04-28 17:14:25 +0300

    modified backend.tf and subnets.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "ta-challenge-elk-team-3"
     key    = "sprint3/challenge/elk-stack/terraform.tfstates"
+    region = "us-east-2"
     #dynamodb_table = "terraform-lock=false"
   }
 }
--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet""public-elk" {
     vpc_id = aws_vpc.main.id
     cidr_block = "192.168.1.0/24"
-    availability_zone = "us-east-2"
+    availability_zone = "us-east-2a"
     map_public_ip_on_launch = true
 
     tags = {
@@ -12,7 +12,7 @@ resource "aws_subnet""public-elk" {
 resource "aws_subnet""private-elk" {
     vpc_id = aws_vpc.main.id
     cidr_block = "192.168.2.0/24"
-    availability_zone = "us-east-2"
+    availability_zone = "us-east-2a"
 
     tags = {
         Name = "talent-academy-elk-private-a"
